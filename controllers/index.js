e2a2c5a912924c0b1c0c89b66888c5e92a914f36
Pedromd99 <40790249+Pedromd99@users.noreply.github.com>
2019-08-02 11:23:40 +0200

    cambios

---
+++ controllers/index.js
@@ -0,0 +1,26 @@
+/* const index = 
+
+const ListofProducts = (
+
+const newProduct = 
+
+
+const items = [{
+        id: 1,
+        name: 'product1'
+    },
+    {
+        id: 2,
+        name: 'product2'
+    },
+    {
+        id: 3,
+        name: 'product3'
+    }
+];
+
+module.exports = {
+    index,
+    ListofProducts,
+    newProduct
+} */
